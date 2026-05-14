366902e6522c63d2992e5e56d966f5b77bcdc13b
tongkaibao <53322086+tongkaibao@users.noreply.github.com>
2026-05-14 23:05:12 +0800

    Update post.ts

--- src/data/post.ts
+++ src/data/post.ts
@@ -1,9 +1,13 @@
 import { type CollectionEntry, getCollection } from "astro:content";
 
 /** filter out draft posts based on the environment */
-export async function getAllPosts(): Promise<CollectionEntry<"post">[]> {
-	return await getCollection("post", ({ data }) => {
-		return import.meta.env.PROD ? !data.draft : true;
+export function sortMDByDate(posts: Array<CollectionEntry<"post">>) {
+	return posts.sort((a, b) => {
+		const aDate = new Date(a.data.updatedDate ?? a.data.publishDate).getTime();
+		const bDate = new Date(b.data.updatedDate ?? b.data.publishDate).getTime();
+		// 返回 b - a 表示从大到小（最新的在前）
+		// 如果你想从旧到新，就改为 aDate - bDate
+		return bDate - aDate;
 	});
 }
 
